2a8a5d459783744db0ed521342c0ddca4c11bc9b
Your Name <mosher@mosher.com>
2023-09-13 21:32:35 +0300

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cocmd/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'cocmd', // Usually your GitHub org/user name.
-  projectName: 'cocmd', // Usually your repo name.
+  projectName: 'website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
