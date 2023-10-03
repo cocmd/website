9ffb7ab3486386696faa95b1277359726b9365d5
Your Name <you@example.com>
2023-10-03 22:49:23 +0300

    Deploy website - based on b899ceddaed9507252fa8fcf1e015d4505a41f30

---
+++ linux/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define variables
+GITHUB_USER="cocmd"
+REPO_NAME="cocmd"
+RELEASE_TAG="v1.0.9"
+DEB_PACKAGE_NAME="cocmd-debian-x11-amd64.deb"
+
+# Install libssl1.1 (if needed)
+
+sudo apt-get update
+sudo apt-get install -y libssl-dev
+
+
+# Download the .deb package from the GitHub release
+wget "https://github.com/${GITHUB_USER}/${REPO_NAME}/releases/download/${RELEASE_TAG}/${DEB_PACKAGE_NAME}"
+
+# Install the .deb package
+sudo dpkg -i ${DEB_PACKAGE_NAME}
+
+# Install any missing dependencies (if needed)
+sudo apt-get -f install -y
+
+# Clean up the downloaded .deb file
+rm ${DEB_PACKAGE_NAME}
+
+# Add cocmd to the user's PATH
+echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+echo "Cocmd has been successfully installed and added to your PATH!"
